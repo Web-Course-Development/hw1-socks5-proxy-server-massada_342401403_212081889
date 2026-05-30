5e47c6ab0fe68349457ceb69691c630c7dd757bf
Omri Tamari <omri.tamari@gmail.com>
2026-05-30 17:58:16 +0300

    finished getting adress from client

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"encoding/binary"
 )
 
 func main() { 
@@ -30,15 +31,15 @@ func main() {
 	}
 }
 
-// the idea: 
-// check 0x05 for SOCKS5 -> check 0x00 OR 0x02 for username\pass
-// if 0x02 in methods -> jump into authenticateUserPass()
-// if 0x00 or after authenticateUser -> jump into handleConnection()
-// after handleConnection -> jump into relay()
+	// TODO: Implement SOCKS5 protocol
+	// 1. V- Read client greeting and negotiate authentication method
+	// 2. V- Perform authentication if required (when PROXY_USER env var is set)
+	// 3. Read CONNECT request
+	// 4. Connect to target server
+	// 5. Send success/error reply
+	// 6. V- Relay data between client and target
 
-
-
-// returns error or nil
+// returns error or nil if negotiation went well
 func negotiateAuth(conn net.Conn) error { 
 	header := make([]byte, 2)
 	if _, err := io.ReadFull(conn, header); err != nil {
@@ -95,7 +96,7 @@ func negotiateAuth(conn net.Conn) error {
 	return nil
 }
 
-// returns error or nil
+// returns error or nil if authentication went well
 func authenticateUserPass(conn net.Conn) error {
 	header := make([]byte, 2) // taking in version and userLen
 	if _, err := io.ReadFull(conn, header); err != nil {
@@ -103,6 +104,7 @@ func authenticateUserPass(conn net.Conn) error {
 	}
 	ver, usernameLen := header[0], header[1]
 	if ver != 0x01 {
+		conn.Close()
 		return errors.New("not username password method")
 	}
 	username := make([]byte, usernameLen)
@@ -150,18 +152,79 @@ func authenticateUserPass(conn net.Conn) error {
 
 }
 
-// 
-func handleConnection(conn net.Conn) {
+// returns error or nil if connectionwent wel
+func handleConnection(conn net.Conn) error {
 	defer conn.Close()
+	err := negotiateAuth(conn); // immedietly go check SOCKS5 and the username password thingy
+	if(err!=nil){ // returned with issue
+		// not closing connection since it's already bene closed earlier
+		return err
+	}
+	
+	header := make([]byte, 4)
+	if _, err := io.ReadFull(conn, header); err != nil {
+		return err
+	}
+	if(header[0]!=0x05 || header[1] != 0x01 || header[2] != 0x00){
+		// at least ONE of the first bytes isnt good
+		conn.Close()
+		return errors.New("issue in the connection attempt")
+	} else{
+		adress := ""
+		if(header[3] == 0x01){
+			ipArr := make([]byte, 4)
+			if _, err := io.ReadFull(conn, ipArr); err != nil {
+				return err
+			}
+			adress = net.IP(ipArr).String()
+		} else if(header[3] == 0x03) {
+			domainLength := make([]byte, 1)
+			if _, err := io.ReadFull(conn, domainLength); err != nil {
+				return err
+			}
+			domainArr := make([]byte, int(domainLength[0]))
+			if _, err := io.ReadFull(conn, domainArr); err != nil {
+				return err
+			}
+			adress = string(domainArr)
+		} else {
+			// not IPv4 and not domain: error
+			conn.Close()
+			return errors.New("issue in ATYP (not domain and not IPv4)")
+		}
+		port := make([]byte, 2)
+		if _, err := io.ReadFull(conn, port); err != nil {
+			return err
+		}
+		portNum := binary.BigEndian.Uint16(port)
+		finalAdress := fmt.Sprintf("%s:%d", adress, portNum)
+		// if everything so far is okay: reply about the connection
 
-	negotiateAuth(conn); // immedietly go check SOCKS5 and the username password thingy
-
-
-	// TODO: Implement SOCKS5 protocol
-	// 1. Read client greeting and negotiate authentication method
-	// 2. Perform authentication if required (when PROXY_USER env var is set)
-	// 3. Read CONNECT request
-	// 4. Connect to target server
-	// 5. Send success/error reply
-	// 6. Relay data between client and target
+	}
 }
+
+
+func relay(client net.Conn, target net.Conn) {
+	//adapting the concurrent relay function to the full signature to prevent TCP Deadlock 
+	var wg sync.WaitGroup
+	wg.Add(2)
+	//client -> target
+	go func() {
+		defer wg.Done()
+		_, _ = io.Copy(target, client)
+		if tcpConn, ok := target.(*net.TCPConn); ok {
+			_ = tcpConn.CloseWrite() 
+		}
+	}()
+	//target -> client
+	go func() {
+		defer wg.Done()
+		_, _ = io.Copy(client, target)
+		if tcpConn, ok := client.(*net.TCPConn); ok {
+			_ = tcpConn.CloseWrite() 
+		}
+	}()
+	wg.Wait()
+} 
+
+
